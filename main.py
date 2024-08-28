37effd4fcc9dabc441b67b3edfdb9a500b4e2cf6
COTE <96086982+orhogi@users.noreply.github.com>
2024-08-28 12:02:17 +0300

    main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = ""
+TOKEN = "6803830590:AAFcrKezpRIOnQpocN4FLUkiQm_Mgsi0rRM"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
